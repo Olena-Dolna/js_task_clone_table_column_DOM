8d52c58ff05e80d03faa599603224f33aa49b6dd
Olena Dolna <dolnaolena2019@gmail.com>
2026-01-28 14:06:03 +0100

    fixed typos in variable names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const oldTable = document.querySelector('table');
-const newColumHeadData = oldTable
+const newColumnHeadData = oldTable
   .querySelector('thead > tr > th:nth-child(2)')
   .cloneNode(true);
-const newColumFootData = oldTable
+const newColumnFootData = oldTable
   .querySelector('tfoot > tr > th:nth-child(2)')
   .cloneNode(true);
 const copiedColumnData = Array.from(
@@ -14,8 +14,8 @@ const lastHeadCell = oldTable.querySelector('thead > tr > th:last-child');
 const lastFootCell = oldTable.querySelector('tfoot > tr > th:last-child');
 const lastColumnData = oldTable.querySelectorAll('tr > td:last-child');
 
-lastHeadCell.before(newColumHeadData);
-lastFootCell.before(newColumFootData);
+lastHeadCell.before(newColumnHeadData);
+lastFootCell.before(newColumnFootData);
 
 for (let i = 0; i < copiedColumnData.length; i++) {
   lastColumnData[i].before(copiedColumnData[i].cloneNode(true));
